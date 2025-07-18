edf4cd60c4ac7ffa1f73a375c8dbe04bdde04140
Ankush115 <alwagh.45@gmail.com>
2025-07-18 17:53:01 +0530

    latest commit

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,49 +1,78 @@
 import axios from "axios";
-import { useRef } from "react";
+import { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Register = () => {
-  const dataObj = useRef({});
+  const [dataObj, setDataObj] = useState({
+    name: "",
+    location: "",
+    phone: "",
+    email: "",
+  });
+
   const handleChange = (eve) => {
     const { id, value } = eve.target;
-
-    dataObj.current = {
-      ...dataObj.current,
+    setDataObj({
+      ...dataObj,
       [id]: value,
-    };
+    });
   };
+
   const handleRegister = () => {
-    const { name, loc, phone, email } = dataObj.current;
+    const { name, location, phone, email } = dataObj;
+
     axios
-      .post("http://localhost:3000/users/reg", { name, loc, phone, email }, {})
+      .post("http://localhost:3000/users/reg", { name, location, phone, email })
       .then((res) => {
-        console.log("Success", res);
+        const { acknowledged, insertedId } = res.data;
+        if (acknowledged && insertedId) {
+          setDataObj({ name: "", location: "", phone: "", email: "" });
+          alert("Saved");
+        } else {
+          alert("Not Saved");
+        }
       })
-      .catch((res) => {
-        console.log("Faillure", res);
-      })
-      
+      .catch(() => {
+        alert("Something went wrong");
+      });
   };
+
   return (
-    <div>
-      <p>
-        <b>Name:</b>
-        <input id="name" onChange={handleChange} />
-      </p>
-      <p>
-        <b>Location:</b>
-        <input id="location" onChange={handleChange} />
-      </p>
-      <p>
-        <b>Phone:</b>
-        <input id="phone" onChange={handleChange} />
-      </p>
-      <p>
-        <b>Email:</b>
-        <input id="email" onChange={handleChange} />
-      </p>
-      <p>
-        <button onClick={handleRegister}>Register</button>
-      </p>
+    <div className="container-sm m-5 d-inline-block border border-1 border-dark bg-info">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+
+          {/* Form Group: Name */}
+          <div className="m-3 d-flex align-items-center">
+            <label htmlFor="name" className="form-label"><b>Name:</b></label>
+            <input id="name" className="form-control" value={dataObj.name} onChange={handleChange} />
+          </div>
+
+          {/* Form Group: Phone */}
+          <div className="m-3 d-flex align-items-center">
+            <label htmlFor="phone" className="form-label"><b>Phone:</b></label>
+            <input id="phone" className="form-control" value={dataObj.phone} onChange={handleChange} />
+          </div>
+
+          {/* Form Group: Email */}
+          <div className="m-3 d-flex align-items-center">
+            <label htmlFor="email" className="form-label "><b>Email:</b></label>
+            <input id="email" className="form-control" value={dataObj.email} onChange={handleChange} />
+          </div>
+
+          {/* Form Group: Location */}
+          <div className="m-3 d-flex align-items-center">
+            <label htmlFor="location" className="form-label"><b>Location:</b></label>
+            <input id="location" className="form-control" value={dataObj.location} onChange={handleChange} />
+          </div>
+
+          {/* Register Button */}
+          <div className="text-center">
+            <button className="btn btn-primary m-3" onClick={handleRegister}>Register</button>
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 };
